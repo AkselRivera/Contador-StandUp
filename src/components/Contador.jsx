064888d9168f5182303d074335564571cba55be1
xlboy360 <juan_jsc360@hotmail.com>
2022-07-02 20:54:32 -0500

    Ultimos cambios

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,77 +1,68 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
 
+import start from "../images/standUp.jpg";
 
-import start from '../images/standUp.jpg';
+const Timer = ({ id }) => {
+  useEffect(() => {
+    console.log("Contaodr", id);
+  }, []);
 
-const Timer = ({initialSeconds}) => {
+  const [minutes, setMinutes] = useState(id === 1 ? 3 : 2);
+  const [seconds, setSeconds] = useState(0);
 
-    const Navigate= useNavigate();
-    const params = useParams();
-    const {id} = params;
-
-
-    
-    const [ minutes, setMinutes ] = useState(  id==1 ? 3 : 2  );
-    const [seconds, setSeconds ] =  useState(initialSeconds=0);
-
-    const useHandleClick = ()=>{
-        Navigate('/',{ replace:true })
-    }
-
-    useEffect(()=>{
+  useEffect(() => {
     let myInterval = setInterval(() => {
-            if (seconds > 0) {
-                setSeconds(seconds - 1);
-            }
-            if (seconds === 0) {
-                if (minutes === 0) {
-                    clearInterval(myInterval)
-                } else {
-                    setMinutes(minutes - 1);
-                    setSeconds(59);
-                }
-            } 
-        }, 1000)
-        
-        return ()=> {
-            clearInterval(myInterval);
-        };
-    }, [minutes, seconds]);
-
-
-    return (
-        <div>
-        { minutes === 0 && seconds === 0
-            ? 
-            <div className='container text-center'>
-                <div className='p-5 align-middle'>
-                    <h1 className='display-1'> 0:00 </h1>
-                    <h1 className='display-3'> ¡Tiempo agotado! </h1>
-                    <img src={start} alt='' width='50%' className='counterImage mt-2 rounded shadow-lg' />
-
-                    <div className='row mx-auto mt-2'>
-                    <div>
-                    <button 
-                        className='p-3 btn btn-outline-light btn-lg'
-                        onClick={useHandleClick}
-                        >Regresar
-                    </button>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            : 
-            <div className='container text-center'>
-                <div className='p-5 align-middle'>
-                    <h1 className='display-1'> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1>
-                    <img src={start} alt='' width='50%' className='counterImage rounded shadow-lg mt-5' />
-                </div>
-            </div>
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
         }
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, [minutes, seconds]);
+
+  return (
+    <div>
+      {minutes === 0 && seconds === 0 ? (
+        <div className="container text-center">
+          <div className="p-5 align-middle">
+            <h1 className="display-1"> ¡Tiempo agotado! </h1>
+            <img
+              src={start}
+              alt=""
+              width="50%"
+              className="counterImage mt-2 rounded shadow-lg"
+            />
+          </div>
         </div>
-    )
-}
+      ) : (
+        <div className="container text-center">
+          <div className="p-5 align-middle">
+            <h1 className="display-1">
+              {" "}
+              {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+            </h1>
+            <img
+              src={start}
+              alt=""
+              width="50%"
+              className="counterImage rounded shadow-lg mt-5"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Timer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,58 +1,102 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import welcome from '../images/welcome.jpg';
-import loading from '../images/loading.gif';
+import React, { useState } from "react";
+import Timer from "../components/Contador";
 
-import win from '../images/win.jpg';
-import lose from '../images/lose.jpg';
+import welcome from "../images/welcome.jpg";
+import cozumelLogo from "../images/cozumel-logo.png";
+import bmgLogo from "../images/bmg-logo.jpeg";
+import loose from "../images/Loose.jpeg";
+import win from "../images/Win.jpeg";
+import gif from "../images/stand-up.gif";
 
 const Home = () => {
+  const [imageRender, setImageRender] = useState(welcome);
+  const [clicked, setClicked] = useState(false);
+  const [resultado, setResultado] = useState(0);
+  const [titulo, setTitulo] = useState("Time out Open Mic");
 
-    const Navigate =useNavigate();
+  function timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 
-    const [imageRender, setImageRender] = useState(welcome);
-    const [clicked, setClicked] = useState(false)
-
-    function timeout(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+  const handleClick = async () => {
+    const rann = Math.floor(Math.random() * 2);
+    // setImageRender(loading);
+    setResultado(rann);
+    console.log("Home ", resultado);
+    console.log("Rann ", rann);
+    setImageRender(gif);
+    await timeout(2000);
+    if (rann === 1) {
+      setImageRender(win);
+      setTitulo("Ganaste!");
+      setImageRender(win);
+    } else {
+      setTitulo("Perdiste!");
+      setImageRender(loose);
     }
-    
 
-    const handleClick = async ()=>{
-        setClicked(true);
-        setImageRender(loading);
-            await timeout(2000);
-                const resultado = Math.floor(Math.random() * (2 - 0)) + 1;
-                console.log(resultado);
-                if(resultado===1)
-                setImageRender(win);
-                else
-                setImageRender(lose);
-                
-            await timeout(2000);
-            Navigate(`/presentacion/${resultado}`)
-                setClicked(false);
-    }
+    await timeout(2000);
+    setClicked(true);
+  };
 
+  const handleCancel = () => {
+    setClicked(false);
+    setTitulo("Time out Open Mic");
+    setImageRender(welcome);
+    setResultado(0);
+  };
 
-return (
-    <div className='text-center'>
-        <div className='py-1'>
-            <h1 className='display-1 '>Titulo de la empresa</h1>
+  return (
+    <div className="text-center container-fluid w-100">
+      <div className="f-flex align-items-center">
+        <div className="d-flex justify-content-evenly">
+          <img
+            className="lobbyImage rounded shadow-lg "
+            src={bmgLogo}
+            alt=""
+            height={200}
+          />
+          <img src={cozumelLogo} alt="" height={200} />
         </div>
-
-        <div className='px-5'>
-        <div className=''>
-            <img className='lobbyImage rounded shadow-lg ' src={imageRender} alt='' width='50%'/>
+        <div className="d-flex justify-content-center border-top m-5 rounded-2 rounded-2 p-4">
+          <div className="d-flex align-items-center justify-content-center w-50">
+            {!clicked ? (
+              <button
+                className="btn btn-outline-light p-5 btn-lg fs-1"
+                onClick={handleClick}
+              >
+                Comenzar
+              </button>
+            ) : (
+              <button
+                className="btn btn-outline-light p-5 btn-lg fs-1"
+                onClick={handleCancel}
+              >
+                Cancelar
+              </button>
+            )}
+          </div>
+          <div className="flex-fill  h-100 d-inline-block  ">
+            {clicked ? (
+              <Timer id={resultado} />
+            ) : (
+              <div className="container text-center">
+                <div className="p-5 align-middle">
+                  <h1 className="display-1"> {titulo} </h1>
+                  <img
+                    src={imageRender}
+                    alt=""
+                    width="50%"
+                    className="counterImage rounded shadow-lg mt-5"
+                  />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-            {
-            !clicked &&
-            <button className='btn btn-outline-light btn-lg m-3' onClick={handleClick} >Comenzar</button>
-            }
-        </div>
+      </div>
     </div>
-)
-}
+  );
+};
 
-
-export default Home
+export default Home;
